16ee2124802e0a6a05620398ca160fd964b0a65b
Jinser Kafka <aimer@purejs.icu>
2024-09-06 15:01:04 +0800

    add script to generate tests

---
+++ generate-tests.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+gen_tests() {
+    local forest_path=$1
+    local generated=$2
+
+    : > $generated
+
+    forest=$(find $forest_path -name '*.tree' -not -path '*/index.tree')
+    for tree in $forest; do
+        echo "==================" >> $generated
+        echo $(basename $tree) >> $generated
+        echo "==================" >> $generated
+        echo "" >> $generated
+
+        cat $tree >> $generated
+
+        echo "" >> $generated
+        echo "------------------" >> $generated
+        echo "" >> $generated
+
+        tree-sitter test --update --include $(basename $tree) || exit 1
+    done
+}
+
+forest_path="test/golden-forest/trees/"
+generated="test/corpus/all-the-syntax.txt"
+gen_tests $forest_path $generated
+
+forest_path="test/jms-trees/"
+generated="test/corpus/jms-trees.txt"
+gen_tests $forest_path $generated
