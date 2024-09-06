42867890ddb4d19c94721cbb72111a74db6d5c64
Jinser Kafka <aimer@purejs.icu>
2024-09-06 15:46:22 +0800

    add script to pull subtrees

---
+++ script/pull-subtree.sh
@@ -0,0 +1,2 @@
+git subtree pull --prefix test/jms-trees/ git@git.sr.ht:~jonsterling/public-trees.git main --squash
+git subtree pull --prefix test/golden-forest/ git@github.com:jetjinser/golden-forest.git main --squash
