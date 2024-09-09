03da41b9030f83f73f682186d8add83dc1c0c28d
Jinser Kafka <aimer@purejs.icu>
2024-09-09 10:46:24 +0800

    remove unused pull-subtree script

--- script/pull-subtree.sh
+++
@@ -1,2 +0,0 @@
-git subtree pull --prefix test/jms-trees/ git@git.sr.ht:~jonsterling/public-trees main --squash
-git subtree pull --prefix test/golden-forest/ git@github.com:jetjinser/golden-forest.git main --squash
